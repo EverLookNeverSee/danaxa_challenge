75f843beb881310e37d6ffcbcac11380dc0880b1
EverLookNeverSee <EverLookNeverSee@ProtonMail.ch>
2021-07-17 20:50:52 +0430

    Adding initial docstring

--- danaxa_challenge/main.py
+++ danaxa_challenge/main.py
@@ -1,2 +1,9 @@
+"""
+    Main file that contains following items:
+        - Configuring and loading Mask-RCNN Model
+        - Testing Labeling functionality
+"""
+
+
 from mrcnn import model, config
 from cv2 import VideoCapture, cvtColor, COLOR_BGR2RGB
