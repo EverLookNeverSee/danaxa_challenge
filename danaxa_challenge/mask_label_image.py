0321c8c2a4fec3c16218a8b5ff5080bb790f27de
EverLookNeverSee <EverLookNeverSee@ProtonMail.ch>
2021-07-17 22:18:03 +0430

    Read and convert image

--- danaxa_challenge/mask_label_image.py
+++ danaxa_challenge/mask_label_image.py
@@ -6,3 +6,9 @@
 from mrcnn import visualize
 from cv2 import imread, cvtColor, COLOR_BGR2RGB
 from danaxa_challenge.main import model, CLASS_NAMES
+
+
+file_path = "images/test_image.jpeg"
+# Reading image and converting int channels
+bgr_image = imread(file_path)
+rgb_image = cvtColor(bgr_image, COLOR_BGR2RGB)
