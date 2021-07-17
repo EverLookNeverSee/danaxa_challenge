3a9100f909e23a41547fbfbb32d6ef3dd71db6ae
EverLookNeverSee <EverLookNeverSee@ProtonMail.ch>
2021-07-17 22:16:55 +0430

    Importing relevant modules

--- danaxa_challenge/mask_label_image.py
+++ danaxa_challenge/mask_label_image.py
@@ -1,3 +1,8 @@
 """
     Testing mask and label functionality on test image
 """
+
+
+from mrcnn import visualize
+from cv2 import imread, cvtColor, COLOR_BGR2RGB
+from danaxa_challenge.main import model, CLASS_NAMES
