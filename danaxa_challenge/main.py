d179fe7548e578ee4e23ea7ab4f27e31989c6ee0
EverLookNeverSee <EverLookNeverSee@ProtonMail.ch>
2021-07-17 20:51:37 +0430

    Implementing model configuration class

--- danaxa_challenge/main.py
+++ danaxa_challenge/main.py
@@ -7,3 +7,11 @@
 
 from mrcnn import model, config
 from cv2 import VideoCapture, cvtColor, COLOR_BGR2RGB
+
+
+class MainConfig(config.Config):
+    """ Model Configuration Class """
+    NAME = "coco_inference"
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+    NUM_CLASSES = 81
