787b93290fb372c20cbfd5667ce6c8b237c14a14
EverLookNeverSee <EverLookNeverSee@ProtonMail.ch>
2021-07-17 22:20:23 +0430

    Visualizing masks

--- danaxa_challenge/mask_label_image.py
+++ danaxa_challenge/mask_label_image.py
@@ -18,3 +18,11 @@ result = results[0]
 # Extracting label names instead of indexes
 detected_labels = [CLASS_NAMES[item] for item in result.get("class_ids")]
 print(f"Labels: {detected_labels}")
+
+# Visualizing masks
+visualize.display_instances(image=rgb_image,
+                            boxes=result['rois'],
+                            masks=result['masks'],
+                            class_ids=result['class_ids'],
+                            class_names=CLASS_NAMES,
+                            scores=result['scores'])
